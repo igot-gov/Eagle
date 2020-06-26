d453b091b6b71f1891f5dee0895259deca38d7df
christopher.fernandes <christyabcd.05@gmail.com>
2020-06-27 00:55:39 +0530

    Task #50801: Redirect uri added to email sent

--- apps/web-services/src/utils/keycloak-user-creation.ts
+++ apps/web-services/src/utils/keycloak-user-creation.ts
@@ -182,10 +182,12 @@ export async function sendActionsEmail(userId: string) {
         realmName: CONSTANTS.KEYCLOAK_REALM,
     })
     logInfo(`Sending email to ${userId}`)
+    logInfo(`redirect Uri: `, CONSTANTS.HTTPS_HOST)
     return kcAdminClient.users.executeActionsEmail({
         actions: [RequiredActionAlias.VERIFY_EMAIL],
         id: userId,
         lifespan: 43200,
+        redirectUri: CONSTANTS.HTTPS_HOST,
     })
         // tslint:disable-next-line: no-any
         .then((resp: any) => {
