81e313be9c920799cba9e7cb8fcd6bbd955a6295
Amit Kumar <amit.sengar@tarento.com>
2020-07-15 10:31:50 +0530

    Draft status and Nav changes

--- apps/fusion/library/ws-widget/collection/src/lib/_common/display-contents/display-contents.module.ts
+++ apps/fusion/library/ws-widget/collection/src/lib/_common/display-contents/display-contents.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { DisplayContentsComponent } from './display-contents.component'
 import { DisplayContentTypeModule } from '../display-content-type/display-content-type.module'
-import { PipeDurationTransformModule, PipeLimitToModule } from '@ws-widget/utils'
-import { MatIconModule, MatButtonModule, MatProgressBarModule } from '@angular/material'
+import { PipeDurationTransformModule, PipeLimitToModule, DefaultThumbnailModule } from '@ws-widget/utils'
+import { MatIconModule, MatButtonModule, MatProgressBarModule, MatCardModule } from '@angular/material'
 import { RouterModule } from '@angular/router'
 
 @NgModule({
   declarations: [DisplayContentsComponent],
   imports: [
     CommonModule,
+    DefaultThumbnailModule,
     DisplayContentTypeModule,
     PipeDurationTransformModule,
     PipeLimitToModule,
+    MatCardModule,
     RouterModule,
     MatIconModule,
     MatButtonModule,
