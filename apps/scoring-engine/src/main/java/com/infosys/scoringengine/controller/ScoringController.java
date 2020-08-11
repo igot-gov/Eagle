c5e37686610cc904dd16fc624c4c784960cfffa9
pritha-tarento <pritha.chattopadhyay@tarento.com>
2020-08-11 19:38:13 +0530

    Intial commit for  scoring-engine module

---
+++ apps/scoring-engine/src/main/java/com/infosys/scoringengine/controller/ScoringController.java
@@ -0,0 +1,16 @@
+/*
+ *                "Copyright 2020 Infosys Ltd.
+ *                Use of this source code is governed by GPL v3 license that can be found in the LICENSE file or at https://opensource.org/licenses/GPL-3.0
+ *                This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License version 3"
+ *
+ */
+package com.infosys.scoringengine.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/scoring")
+public class ScoringController {
+
+
+}
