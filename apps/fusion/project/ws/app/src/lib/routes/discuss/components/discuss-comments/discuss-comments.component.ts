581cf7ec99118ca9c7b9bb3ad9e4d79166a3ed4a
Jenifer Ramsingh <jeniferramsingh@gmail.com>
2020-09-07 16:50:25 +0530

    error fixes in the discuss compoennt

--- apps/fusion/project/ws/app/src/lib/routes/discuss/components/discuss-comments/discuss-comments.component.ts
+++ apps/fusion/project/ws/app/src/lib/routes/discuss/components/discuss-comments/discuss-comments.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./discuss-comments.component.scss'],
 })
 export class DiscussCommetsComponent implements OnInit {
-  items = ['1', '2', '3', '4', '5']
+  items = [
+    'All new methods of control of powers of the administrative authorities and more such policies.',
+    'Powers and functions of the administrative authorities Methods of control of powers of the administrative authorities',
+    'New procedures to be followed by the administrative authorities',
+    'India’s first line of workers is already engaged in training module for management of COVID 19']
   ngOnInit(): void {
 
   }
