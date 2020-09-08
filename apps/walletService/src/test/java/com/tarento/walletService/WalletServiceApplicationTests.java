4e74e756d57aed32fc7552eb5125b0adb5df9367
karthik-tarento <karthikeyan.rajendran@tarento.com>
2020-09-08 14:56:50 +0530

    Initial commit for Wallet-Service

---
+++ apps/walletService/src/test/java/com/tarento/walletService/WalletServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarento.walletService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
