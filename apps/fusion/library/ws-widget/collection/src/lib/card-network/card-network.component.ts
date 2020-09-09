f3aa44420694cb644300a05998b2b2eb60f60349
Jenifer Ramsingh <jeniferramsingh@gmail.com>
2020-09-09 09:56:20 +0530

    Pipetranformation is added for name view and other UI fixes also code cleaning

--- apps/fusion/library/ws-widget/collection/src/lib/card-network/card-network.component.ts
+++ apps/fusion/library/ws-widget/collection/src/lib/card-network/card-network.component.ts
@@ -1,11 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core'
 import { Router } from '@angular/router'
 import { NsWidgetResolver, WidgetBaseComponent } from '@ws-widget/resolver'
-import { ConfigurationsService } from '@ws-widget/utils'
 
-import { ActivitiesService } from '@ws/app/src/lib/routes/activities/services/activities.service'
-import { IActivity, IActivityCard, IChallenges } from '@ws/app/src/lib/routes/activities/interfaces/activities.model'
-import { MatSnackBar } from '@angular/material'
 
 @Component({
   selector: 'ws-widget-card-welcome',
@@ -16,14 +12,17 @@ export class CardNetworkComponent extends WidgetBaseComponent
 
   implements OnInit, NsWidgetResolver.IWidgetData<any> {
   enableFeature = true
+  enablePeopleSearch = true
   @Input() widgetData: any
   givenName: string | undefined
   userEmail: string | undefined
-  activityCards: IActivityCard[] = []
-  challenges: IChallenges[] = []
-  isNewUser = false
-  showActivities = false
   keyTag: string[] = []
+
+  constructor(private router: Router) {
+
+    super()
+
+  }
 
   cardArray = [{
     first_name: 'Amit',
@@ -69,70 +68,53 @@ export class CardNetworkComponent extends WidgetBaseComponent
     designation: '',
     userLocation: '',
     city: '',
+  }, {
+    first_name: 'Amit',
+    last_name: 'Yadav',
+    email: 'amitkumar.yadav@tarento.com',
+    desc: 'Last active 8 days ago',
+    user_id: null,
+    department: 'igot',
+    phone_No: '0',
+    designation: '',
+    userLocation: '',
+    city: '',
+  }, {
+    first_name: 'Amit',
+    last_name: 'Yadav',
+    email: 'amitkumar.yadav@tarento.com',
+    desc: 'Last active 8 days ago',
+    user_id: null,
+    department: 'igot',
+    phone_No: '0',
+    designation: '',
+    userLocation: '',
+    city: '',
+  }, {
+    first_name: 'Amit',
+    last_name: 'Yadav',
+    email: 'amitkumar.yadav@tarento.com',
+    desc: 'Last active 8 days ago',
+    user_id: null,
+    department: 'igot',
+    phone_No: '0',
+    designation: '',
+    userLocation: '',
+    city: '',
+  }, {
+    first_name: 'Amit',
+    last_name: 'Yadav',
+    email: 'amitkumar.yadav@tarento.com',
+    desc: 'Last active 8 days ago',
+    user_id: null,
+    department: 'igot',
+    phone_No: '0',
+    designation: '',
+    userLocation: '',
+    city: '',
   }]
-  constructor(
-    private configSvc: ConfigurationsService,
-    private router: Router,
-    private activitiesSvc: ActivitiesService,
-    private snackBar: MatSnackBar,
-  ) {
-    super()
-    if (this.configSvc.userProfile) {
-      this.givenName = this.configSvc.userProfile.givenName
-      this.userEmail = this.configSvc.userProfile.email
-    }
-    this.isNewUser = this.configSvc.isNewUser
-    if (this.configSvc.restrictedFeatures) {
-      if (this.configSvc.restrictedFeatures.has('activities')) {
-        this.showActivities = false
-      } else {
-        this.showActivities = true
-      }
-    } else {
-      this.showActivities = false
-    }
-
-  }
-  hasRole(role: string[]): boolean {
-    let returnValue = false
-    role.forEach(v => {
-      if ((this.configSvc.userRoles || new Set()).has(v)) {
-        returnValue = true
-      }
-    })
-    return returnValue
-  }
   ngOnInit() {
-    if (this.showActivities) {
-      this.activitiesSvc.fetchActivites().then((result: IActivity) => {
-        if (result.activities.length !== 0) {
-          result.activities.forEach(activity => {
-            if (activity.hasRole.length === 0 || this.hasRole(activity.hasRole)) {
-              this.activityCards.push(activity)
-            }
-          })
-          // this.activityCards = result.activities
-          this.activityCards.forEach(activityCard => {
-            if (!(this.keyTag.includes(activityCard.tag))) {
-              this.keyTag.push(activityCard.tag)
-            }
-          })
-          this.keyTag.forEach(tag => {
-            const filteredActivity = this.activityCards.filter(activity => (tag === activity.tag))
-            this.challenges.push({ tag, activities: filteredActivity })
-          })
-        } else {
-          this.showActivities = false
-        }
-      }).catch(() => {
-        this.showActivities = false
-        this.snackBar.open('Failed to load activities', 'X')
-      })
-    }
-  }
 
-  allActivities() {
-    this.router.navigate(['app', 'activities'])
   }
 
   getUserFullName(user: any) {
@@ -140,6 +122,9 @@ export class CardNetworkComponent extends WidgetBaseComponent
       return `${user.first_name.trim()} ${user.last_name.trim()}`
     }
     return ''
+  }
+  goToUserProfile(user: any) {
+    this.router.navigate(['/app/person-profile'], { queryParams: { emailId: user.email } })
   }
 
 }
--- apps/fusion/library/ws-widget/collection/src/lib/card-network/card-network.module.ts
+++ apps/fusion/library/ws-widget/collection/src/lib/card-network/card-network.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 import { MatButtonModule, MatCardModule, MatChipsModule, MatDividerModule, MatExpansionModule, MatIconModule, MatProgressSpinnerModule } from '@angular/material'
-import { HorizontalScrollerModule } from '@ws-widget/utils'
+import { HorizontalScrollerModule, PipeNameTransformModule } from '@ws-widget/utils'
 import { ActivitiesService } from '../../../../../../project/ws/app/src/lib/routes/activities/services/activities.service'
 import { ActivityCardModule } from '../activity-card/activity-card.module'
 import { TourModule } from '../_common/tour-guide/tour-guide.module'
@@ -24,6 +24,7 @@ import { ChallengeModule } from '../challenge/challenge.module'
     HorizontalScrollerModule,
     ActivityCardModule,
     TourModule,
+    PipeNameTransformModule,
     ChallengeModule,
   ],
   entryComponents: [CardNetworkComponent],
