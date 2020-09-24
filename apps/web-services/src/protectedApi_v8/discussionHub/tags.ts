2d70cb723728114b8114d8568c77cf71314ffadf
Amit Kumar <amit.sengar@tarento.com>
2020-09-24 18:33:32 +0530

    my discussion with tag data=> web proxy

--- apps/web-services/src/protectedApi_v8/discussionHub/tags.ts
+++ apps/web-services/src/protectedApi_v8/discussionHub/tags.ts
@@ -7,6 +7,7 @@ import { logError, logInfo } from '../../utils/logger'
 import { extractUserIdFromRequest } from '../../utils/requestExtract'
 
 const API_ENDPOINTS = {
+    getTagTopics: (tagName: string) => `${CONSTANTS.DISCUSSION_HUB_API_BASE}/api/tags/${tagName}`,
     getTags: `${CONSTANTS.DISCUSSION_HUB_API_BASE}/api/tags`,
 }
 
@@ -25,6 +26,24 @@ tagsApi.get('/', async (req, res) => {
         res.send(response.data)
     } catch (err) {
         logError('ERROR ON GET topicsApi /recent >', err)
+        res.status((err && err.response && err.response.status) || 500)
+            .send(err && err.response && err.response.data || {})
+    }
+})
+tagsApi.get('/:tagName', async (req, res) => {
+    try {
+        const rootOrg = getRootOrg(req)
+        const userId = extractUserIdFromRequest(req)
+        logInfo(`UserId: ${userId}, rootOrg: ${rootOrg}`)
+        const tagName = req.params.tagName
+        const url = API_ENDPOINTS.getTagTopics(tagName)
+        const response = await axios.get(
+            url,
+            { ...axiosRequestConfig, headers: { rootOrg } }
+        )
+        res.send(response.data)
+    } catch (err) {
+        logError('ERROR ON GET topicsApi /by tag >', err)
         res.status((err && err.response && err.response.status) || 500)
             .send(err && err.response && err.response.data || {})
     }
