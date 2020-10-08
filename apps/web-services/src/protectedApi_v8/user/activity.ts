1bc50cd434a772212b3e072fd84e8eb6123616c0
Amit Kumar <amit.sengar@tarento.com>
2020-10-08 18:16:42 +0530

    other user profile view added API

--- apps/web-services/src/protectedApi_v8/user/activity.ts
+++ apps/web-services/src/protectedApi_v8/user/activity.ts
@@ -7,13 +7,13 @@ import { extractUserIdFromRequest } from '../../utils/requestExtract'
 
 const API_END_POINTS = {
   activities: (userId: string) =>
-    `${CONSTANTS.SB_EXT_API_BASE_3}/v1/activities/user/${userId}`
+    `${CONSTANTS.SB_EXT_API_BASE_3}/v1/activities/user/${userId}`,
 }
 
 export const activity = Router()
 
 activity.get('/', async (req, res) => {
-  const wid = extractUserIdFromRequest(req);
+  const wid = extractUserIdFromRequest(req)
 
   const rootOrg = req.header('rootOrg')
   const org = req.header('org')
