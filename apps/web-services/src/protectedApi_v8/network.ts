f64e0c208abb2be1ddf2f2ea6227907740748536
pritha-tarento <42533384+pritha-tarento@users.noreply.github.com>
2020-10-12 21:28:53 +0530

    Update network.ts

--- apps/web-services/src/protectedApi_v8/network.ts
+++ apps/web-services/src/protectedApi_v8/network.ts
@@ -2,45 +2,41 @@ import axios from 'axios'
 import { Router } from 'express'
 import { axiosRequestConfig } from '../configs/request.config'
 import { CONSTANTS } from '../utils/env'
-import { logError } from '../utils/logger'
+import { logError, logInfo } from '../utils/logger'
 import { ERROR } from '../utils/message'
-import { extractUserIdFromRequest } from '../utils/requestExtract'
 
 const unknown = 'Failed due to unknown reason'
 const apiEndpoints = {
-  deleteScromData: `${CONSTANTS.AUTHORING_BACKEND}/action/scrom/delete`,
-  getScromData: `${CONSTANTS.AUTHORING_BACKEND}/action/scrom/fetch`,
-  postScromData: `${CONSTANTS.AUTHORING_BACKEND}/action/scrom/add`,
+  getConnectionRequestsData: `${CONSTANTS.NETWORK_SERVICE_BACKEND}/connections/profile/fetch/requested`,
+  getConnectionEstablishedData: `${CONSTANTS.NETWORK_SERVICE_BACKEND}/connections/profile/fetch/established`,
+  getSuggestedConnectionData: `${CONSTANTS.NETWORK_SERVICE_BACKEND}/connections/profile/find/suggests`,
+  postConnectionRecommendationData: `${CONSTANTS.NETWORK_SERVICE_BACKEND}/connections/profile/find/recommended`,
+  postAddConnectionData: `${CONSTANTS.NETWORK_SERVICE_BACKEND}/connections/add`,
+  postUpdateConnectionData: `${CONSTANTS.NETWORK_SERVICE_BACKEND}/connections/update`,
 }
 
-export const scromApi = Router()
+export const networkConnectionApi = Router()
 
-scromApi.get('/get/:id', async (req, res) => {
-  // logInfo('Scrom=> GET API called=====>', req.params.id || 'id missing')
+networkConnectionApi.get('/get/connections/requested', async (req, res) => {
+  console.log('Connection requests GET API called=====>', req.header('userId') || 'uuid missing')
   try {
-    const userId = extractUserIdFromRequest(req)
-    const org = req.header('org')
     const rootOrg = req.headers.rootorg
-    const contentId = req.params.id
+    const userId = req.header('userId')
 
-    if (!org || !rootOrg) {
+    if (!rootOrg) {
       res.status(400).send(ERROR.ERROR_NO_ORG_DATA)
       return
     }
-    if (!contentId) {
+    if (!userId) {
       res.status(400).send(ERROR.GENERAL_ERR_MSG)
       return
     }
-    const response = await axios.get(apiEndpoints.getScromData, {
+    const response = await axios.get(apiEndpoints.getConnectionRequestsData, {
       ...axiosRequestConfig,
       headers: {
-        org,
         rootOrg,
-      },
-      params: {
-        contentId,
-        userId,
-      },
+        userId
+      }
     })
     res.send((response.data))
 
@@ -54,38 +50,102 @@ scromApi.get('/get/:id', async (req, res) => {
   }
 })
 
-scromApi.post('/add/:id', async (req, res) => {
+networkConnectionApi.get('/get/connections/established', async (req, res) => {
+  console.log('Connection requests GET API called=====>', req.header('userId') || 'uuid missing')
   try {
-    const userId = extractUserIdFromRequest(req)
-    const org = req.header('org') || 'dopt'
-    const rootOrg = req.header('rootorg') || 'igot'
-    const contentId = req.params.id
+    const rootOrg = req.headers.rootorg
+    const userId = req.header('userId')
 
-    // logInfo('org, rootOrg, contentId', org, rootOrg, contentId)
-    if (!org || !rootOrg) {
+    if (!rootOrg) {
       res.status(400).send(ERROR.ERROR_NO_ORG_DATA)
       return
     }
-    if (!contentId) {
+    if (!userId) {
+      res.status(400).send(ERROR.GENERAL_ERR_MSG)
+      return
+    }
+    const response = await axios.get(apiEndpoints.getConnectionEstablishedData, {
+      ...axiosRequestConfig,
+      headers: {
+        rootOrg,
+        userId
+      }
+    })
+    res.send((response.data))
+
+  } catch (err) {
+    logError(err)
+    res.status((err && err.response && err.response.status) || 500).send(
+      (err && err.response && err.response.data) || {
+        error: unknown,
+      }
+    )
+  }
+})
+
+networkConnectionApi.get('/connections/suggests', async (req, res) => {
+  console.log('Connection requests GET API called=====>', req.header('userId') || 'uuid missing')
+console.log("exec - /connections/suggests");
+  try {
+    const rootOrg = req.headers.rootorg
+    const userId = req.header('userId')
+
+    if (!rootOrg) {
+      res.status(400).send(ERROR.ERROR_NO_ORG_DATA)
+      return
+    }
+    if (!userId) {
+      res.status(400).send(ERROR.GENERAL_ERR_MSG)
+      return
+    }
+    const response = await axios.get(apiEndpoints.getSuggestedConnectionData, {
+      ...axiosRequestConfig,
+      headers: {
+        rootOrg,
+        userId
+      }
+    })
+    res.send((response.data))
+
+  } catch (err) {
+    logError('SUGGEST ERROR >', err)
+    res.status((err && err.response && err.response.status) || 500).send(
+      (err && err.response && err.response.data) || {
+        error: unknown,
+      }
+    )
+  }
+})
+
+
+networkConnectionApi.post('/add/connection', async (req, res) => {
+  try {
+    const rootOrg = req.header('rootorg')
+    const connectionId = req.body.connectionId
+    const userId = req.body.userId
+
+
+    // logInfo('org, rootOrg, contentId', org, rootOrg, contentId)
+    if (!rootOrg) {
+      res.status(400).send(ERROR.ERROR_NO_ORG_DATA)
+      return
+    }
+    if (!userId || !connectionId) {
       res.status(400).send(ERROR.GENERAL_ERR_MSG)
       return
     }
 
     const body = req.body
-    body.contentId = contentId
-    body.userId = userId
-
-    // logInfo('body========>', JSON.stringify(body))
+    console.log('body========>', JSON.stringify(body))
 
     const response = await axios.post(
-      apiEndpoints.postScromData,
+      apiEndpoints.postAddConnectionData,
       body,
       {
         ...axiosRequestConfig,
         headers: {
-          org,
           rootOrg,
-        },
+        }
       }
     )
     res.send(response.data)
@@ -99,35 +159,37 @@ scromApi.post('/add/:id', async (req, res) => {
     )
   }
 })
-scromApi.delete('/remove/:id', async (req, res) => {
-  try {
-    const userId = extractUserIdFromRequest(req)
-    const org = req.header('org')
-    const rootOrg = req.headers.rootorg
-    const contentId = req.params.id
 
-    if (!org || !rootOrg) {
+networkConnectionApi.post('/update/connection', async (req, res) => {
+  try {
+    const body = req.body
+    console.log('body========>', JSON.stringify(body))
+
+    const rootOrg = req.header('rootorg')
+    const connectionId = req.body.connectionId
+    const userId = req.body.userId
+    const status = req.body.status
+
+    if (!rootOrg) {
       res.status(400).send(ERROR.ERROR_NO_ORG_DATA)
       return
     }
-    if (!contentId) {
+    if (!userId || !connectionId || !status) {
       res.status(400).send(ERROR.GENERAL_ERR_MSG)
       return
     }
 
-    const response = await axios.post(apiEndpoints.deleteScromData, {}, {
-      ...axiosRequestConfig,
-      headers: {
-        org,
-        rootOrg,
-      },
-      params: {
-        contentId,
-        userId,
-      },
-      timeout: Number(CONSTANTS.KB_TIMEOUT),
-    })
-    res.send((response.data))
+    const response = await axios.post(
+      apiEndpoints.postUpdateConnectionData,
+      body,
+      {
+        ...axiosRequestConfig,
+        headers: {
+          rootOrg,
+        }
+      }
+    )
+    res.send(response.data)
 
   } catch (err) {
     logError(err)
@@ -138,3 +200,39 @@ scromApi.delete('/remove/:id', async (req, res) => {
     )
   }
 })
+
+networkConnectionApi.post('/recommended', async (req, res) => {
+  try {
+    const body = req.body
+    console.log('body========>', JSON.stringify(body))
+
+    const rootOrg = req.header('rootorg') || 'igot'
+
+    if (!rootOrg) {
+      res.status(400).send(ERROR.ERROR_NO_ORG_DATA)
+      return
+    }
+
+    const response = await axios.post(
+      apiEndpoints.postAddConnectionData,
+      body,
+      {
+        ...axiosRequestConfig,
+        headers: {
+          rootOrg,
+        }
+      }
+    )
+    res.send(response.data)
+
+  } catch (err) {
+    logError(err)
+    res.status((err && err.response && err.response.status) || 500).send(
+      (err && err.response && err.response.data) || {
+        error: unknown,
+      }
+    )
+  }
+})
+
+
