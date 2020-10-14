646f8b2dca2ff5210373bb41e39c4074b7571a3b
karthik-tarento <karthikeyan.rajendran@tarento.com>
2020-10-14 15:42:52 +0530

    Fixed mandatory content progress check for author

--- apps/web-services/src/protectedApi_v8/user/mandatoryContent.ts
+++ apps/web-services/src/protectedApi_v8/user/mandatoryContent.ts
@@ -1,7 +1,9 @@
 import axios from 'axios'
 import { Router } from 'express'
-
+import { axiosRequestConfig } from '../../configs/request.config'
 import { CONSTANTS } from '../../utils/env'
+import { logError } from '../../utils/logger'
+import { ERROR } from '../../utils/message'
 
 const API_END_POINTS = {
     mandatoryContentStatus: (userId: string) => `${CONSTANTS.SB_EXT_API_BASE_2}/v1/check/mandatoryContentStatus/${userId}`,
@@ -11,10 +13,25 @@ export const mandatoryContent = Router()
 
 mandatoryContent.get('/checkStatus/:userId', async (req, res) => {
     try {
+        const rootOrg = req.headers.rootorg
+        const org = req.headers.org
+        const wid = req.headers.wid
         const userId = req.params.userId
-        const response = await axios.get(API_END_POINTS.mandatoryContentStatus(userId), req.body)
+        if (!rootOrg || !org || !wid || !userId) {
+            res.status(400).send(ERROR.ERROR_NO_ORG_DATA)
+            return
+        }
+        const response = await axios.get(API_END_POINTS.mandatoryContentStatus(userId), {
+            ...axiosRequestConfig,
+            headers: {
+                org,
+                rootOrg,
+                wid,
+            },
+        })
         res.status(response.status).send(response.data)
     } catch (err) {
+        logError('failed to process the request' + err)
         res.status((err && err.response && err.response.status) || 500).send(
             (err && err.response && err.response.data) || {
                 error: 'Failed due to unknown reason',
