e47fa0e032b23aa7b5253376195c1b87fcb312aa
Amit Kumar <amit.sengar@tarento.com>
2020-10-20 18:37:35 +0530

    fix

--- apps/fusion/project/ws/app/src/lib/routes/competencies/routes/competence-all/competence-all.component.ts
+++ apps/fusion/project/ws/app/src/lib/routes/competencies/routes/competence-all/competence-all.component.ts
@@ -64,7 +64,7 @@ export class CompetenceAllComponent implements OnInit {
   }
 
   reset() {
-    this.searchKey = '
+    this.searchKey = ''
     this.queryControl.setValue('')
     this.selectedId = ''
     this.refreshData()
