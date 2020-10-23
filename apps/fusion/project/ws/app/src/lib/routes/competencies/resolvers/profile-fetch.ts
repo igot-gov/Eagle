40f93e7b1bd285656d3bc24bd1a27db1fe47dee6
Amit Kumar <amit.sengar@tarento.com>
2020-10-23 11:02:27 +0530

    bugs

--- apps/fusion/project/ws/app/src/lib/routes/competencies/resolvers/profile-fetch.ts
+++ apps/fusion/project/ws/app/src/lib/routes/competencies/resolvers/profile-fetch.ts
@@ -11,13 +11,13 @@ import { CompetenceService } from '../services/competence.service'
 export class ProfileResolve
   implements
   Resolve<Observable<IResolveResponse<any>> | IResolveResponse<any>> {
-  constructor(private CompetenceSvc: CompetenceService) { }
+  constructor(private competenceSvc: CompetenceService) { }
 
   resolve(
     _route: ActivatedRouteSnapshot,
     _state: RouterStateSnapshot,
   ): Observable<IResolveResponse<any>> {
-    return this.CompetenceSvc.fetchProfile().pipe(
+    return this.competenceSvc.fetchProfile().pipe(
       map(data => ({ data, error: null })),
       catchError(error => of({ error, data: null })),
     )
