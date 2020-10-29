12b042742d2eb8c47990eee569115c2db8c64d17
karthik-tarento <karthikeyan.rajendran@tarento.com>
2020-10-29 14:14:42 +0530

    Updated the REST path and added params

--- apps/web-services/src/protectedApi_v8/contentValidation.ts
+++ apps/web-services/src/protectedApi_v8/contentValidation.ts
@@ -7,24 +7,26 @@ import { logError } from '../utils/logger'
 import { ERROR } from '../utils/message'
 
 const API_END_POINTS = {
-    checkProfanity: (contentId: string) => `${CONSTANTS.CONTENT_VALIDATION_API_BASE}/v1/profanity/checkProfanity/${contentId}`,
+    checkProfanity: (contentId: string, userId: string) =>
+        `${CONSTANTS.CONTENT_VALIDATION_API_BASE}/contentValidation/v1/checkProfanity/${contentId}/${userId}`,
 }
 
 export const contentValidationApi = Router()
 const unknownError = 'Failed due to unknown reason'
 
-contentValidationApi.get('/checkProfanity/:contentId', async (req, res) => {
+contentValidationApi.get('/checkProfanity/:contentId/:userId', async (req, res) => {
     try {
         const rootOrgValue = req.headers.rootorg
         const orgValue = req.headers.org
         const widValue = req.headers.wid
+        const userId = req.params.userId
         const contentId = req.params.contentId
-        if (!rootOrgValue || !orgValue || !widValue || !contentId) {
+        if (!rootOrgValue || !orgValue || !widValue || !contentId || !userId) {
             res.status(400).send(ERROR.ERROR_NO_ORG_DATA)
             return
         }
 
-        const response = await axios.get(API_END_POINTS.checkProfanity(contentId), {
+        const response = await axios.get(API_END_POINTS.checkProfanity(contentId, userId), {
             ...axiosRequestConfig,
             headers: {
                 org: orgValue,
