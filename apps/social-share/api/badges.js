797d26b41dd810c1668135b87e585edd382171c1
usha <meetsamal.usha@gmail.com>
2020-11-01 20:43:07 +0530

    badge api changes

--- apps/social-share/api/badges.js
+++ apps/social-share/api/badges.js
@@ -1,122 +1,116 @@
 const express = require("express");
 const router = express.Router();
 var request = require("request");
-var _ = require("underscore");
 
-router.get("/badge/:userId/:id", function (req, res) { 
+router.get("/badge/:userId/:id", function (req, res) {
   let URL = `http://lex-core:7001/v3/users/${req.params.userId}/badges`;
-  console.log("api service started",req.params.id);
-  console.log("url",URL);
-  console.log("userId",req.params.userId);
-  let badges,badge;
+  console.log("api service started"); 
+  let badges;
   request(
     {
       headers: {
         local: "en",
-        rootOrg :"igot"
+        rootOrg: "igot",
       },
       uri: URL,
-      method: "GET"     
+      method: "GET"
     },
     function (err, response, body) {
       if (err) {
         console.log("error:", err);
         res.send(err);
       } else {
-        if (typeof body === "string"){
-          JSON.parse(body)
-          badges = body;  
-        }else{
-          badges = body; 
-        }    
+        if (typeof body == "string") {
+          badges = JSON.parse(body);
+        } else {
+          badges = body;
+        }
+
         let badgeArray = [];
-        if(badges.earned && badges.earned.length>0){
-          badgeArray= badges.earned.filter(function(badge){
+        let badge;
+        if (badges.earned && badges.earned.length > 0) {
+          badgeArray = badges.earned.filter(function (badge) {
             return badge.badge_id == req.params.id;
           });
-          badge= badgeArray.length >0 ? badgeArray[0]:null;
+          badge = badgeArray.length > 0 ? badgeArray[0] : null;
         }
         //badge = _.where(badges.earned, {badge_id: req.params.id});
-        
         if (badge) {
           let html = `<!DOCTYPE html>
-            <html lang="en">
-              <head>             
-                <meta charset="utf-8" />
-                <title>"${badge.badge_name}"</title>
-                <link id="id-app-fav-icon" rel="icon" type="image/png" href="/image/favicon.png")
-                " />
-                <meta id="id-social-description" name="description"  content="${
-                  badge.message
-                }">             
-                <meta property="og:type" content="website">
-                <meta property="og:url" content="${`https://d136953gtttd92.cloudfront.net/share/badge/${req.params.userId}/${req.params.id}`}" />
-                <meta property="og:title" content="${badge.badge_name}" />
-                <meta property="og:description" content="${badge.message}" />
-                <meta property="og:image" content="${badge.image}" />  
-                <meta property="og:image:secure_url" content="${badge.image}" />        
-            
-                <meta property="twitter:card" content="summary_large_image" />
-                <meta property="twitter:url" content="${`https://d136953gtttd92.cloudfront.net/share/badge/${req.params.userId}/${req.params.id}`}" />
-                <meta property="twitter:title" content="${badge.badge_name}" />
-                <meta property="twitter:description" content="${badge.message}" />
-                <meta property="twitter:image" content="${badge.image}" />
-                <style>
+          <html lang="en">
+            <head>             
+              <meta charset="utf-8" />
+              <title>"${badge.badge_name}"</title>
+              <link id="id-app-fav-icon" rel="icon" type="image/png" href="/image/favicon.png")
+              " />
+              <meta id="id-social-description" name="description"  content="${
+                badge.message
+              }">             
+              <meta property="og:type" content="website">
+              <meta property="og:url" content="${`https://d136953gtttd92.cloudfront.net/share/badge/${req.params.userId}/${req.params.id}`}" />
+              <meta property="og:title" content="${badge.badge_name}" />
+              <meta property="og:description" content="${badge.message}" />
+              <meta property="og:image" content="${badge.image}" />  
+              <meta property="og:image:secure_url" content="${
+                badge.image
+              }" />        
+          
+              <meta property="twitter:card" content="summary_large_image" />
+              <meta property="twitter:url" content="${`https://d136953gtttd92.cloudfront.net/share/badge/${req.params.userId}/${req.params.id}`}" />
+              <meta property="twitter:title" content="${badge.badge_name}" />
+              <meta property="twitter:description" content="${
+                badge.message
+              }" />
+              <meta property="twitter:image" content="${badge.image}" />
+              <style>
+                .social-card {
+                  box-shadow: 0 0 0 1px rgba(0,0,0,.15), 0 2px 3px rgba(0,0,0,.2);
+                  max-width: 300px;
+                  margin: 16px;
+                  text-align: center;
+                  font-family: arial;
+                }
+                
+                .sub-card {
+                    padding: 10px;
+                  
+                }
+                
+                .title {
+                    font-size: 1.2rem;    
+                    font-weight: 600;
+                    color: rgba(0,0,0,.9);
+                }
+                
+                .desc{
+                  font-size: 0.6rem;  
+                    font-weight: 600;
+                    color: rgba(0,0,0,.9);    
+                }
+                
+                .social-card-img {	
+                  width:100%;   
+                }
+                
                 a {
                   text-decoration: none;
                   font-size: 22px;
                   color: black;
-                  display: flex;
-                }
-                .card-container {
-                  box-shadow: 0 2px 1px -1px rgba(0, 0, 0, 0.2),
-                    0 1px 1px 0 rgba(0, 0, 0, 0.14), 0 1px 3px 0 rgba(0, 0, 0, 0.12);
-                  width: 340px;
-                  height: 120px;
-                  margin: 20px 10px;
-                  display: flex;
-                  padding: 16px;
-                  border-radius: 4px;
-                  font-family: Lato;
-                }
-                .badge-data {
-                  margin-top: 5px;
-                  margin-left: 16px;
-                }
-                .title {
-                  font: 600 16px/24px Montserrat;
-                  margin: 0;
-                }
-                .message {
-                  font: 400 14px/24px Montserrat;
-                }
-                .badge-img {
-                  width: 94px;
-                }
-          
-                .image-container {
-                  box-sizing: border-box;
-                  width: 114px;
-                }             
-                </style>
-              </head>
-              <body>
-              <div class="card-container">
-                  <a href="https://d136953gtttd92.cloudfront.net/app/profile/competency/badges">
-                    <div class="image-container">
-                      <img
-                        class="badge-img"
-                        src="${badge.image}"
-                      />
-                    </div>
-                    <div class="badge-data">
-                      <p class="title">${badge.badge_name}</p>
-                      <p class="message">${badge.message}</p>
-                    </div>
-                  </a> 
-                </div>                  
-              </body>
-            </html>          
+                }                          
+              </style>
+            </head>
+            <body>
+              <div class="social-card">
+                <a href="https://d136953gtttd92.cloudfront.net/app/toc/${req.params.id}/overview">
+                  <img src="${badge.image}" alt="${badge.badge_name}"  class="social-card-img" />
+                  <div class="sub-card">
+                    <pclass="title">${badge.badge_name}</p>
+                    <p class="desc">${badge.message}</p>
+                  </div>  
+                </a>   
+              </div>                       
+            </body>
+          </html>          
               `;
           res.send(html);
         }
