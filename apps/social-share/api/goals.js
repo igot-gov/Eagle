b02c834e4f7cbd17fc437d3c14da15e5cf78509d
usha <meetsamal.usha@gmail.com>
2020-11-05 17:13:41 +0530

    goal api integration

--- apps/social-share/api/goals.js
+++ apps/social-share/api/goals.js
@@ -6,14 +6,12 @@ request.gzip = true;
 router.get("/goal/:userId/:id", function (req, res) {
   console.log("api service started"); 
   let URL = `http://lex-core:7001/v5/users/${req.params.userId}/goals?goal_type=user&sourceFields=isInIntranet`;
-  let data;
+  let goals;
   request(
     {
-      headers: {
-        "content-type": "application/json",
-        rootorg: "igot",
-        org: "dopt",
-        wid: req.params.userId,
+      headers: {        
+        rootorg: "igot",  
+        local: "en",      
       },
       uri: URL,
       method: "GET"     
@@ -23,35 +21,46 @@ router.get("/goal/:userId/:id", function (req, res) {
         console.log("error:", err);
         res.send(err);
       } else {
-        console.log("api successful"); 
-        data = JSON.parse(body);
-        if (data) {
+        if (typeof body == "string") {
+            goals = JSON.parse(body);
+          } else {
+            goals = body;
+          }
+          let goalArray = [];
+          let goal;
+          if (goals.goalsInProgress && goals.goalsInProgress.length > 0) {
+            goalArray = goals.goalsInProgress.filter(function (goal) {
+              return goal.id == req.params.id;
+            });
+            goal = goalArray.length > 0 ? goalArray[0] : null;
+           }
+        if (goal) {
           var html = `<!DOCTYPE html>
             <html lang="en">
               <head>             
                 <meta charset="utf-8" />
-                <title>"${data.name}"</title>
+                <title>"${goal.name}"</title>
                 <link id="id-app-fav-icon" rel="icon" type="image/png" href="/image/favicon.png")
                 " />
                 <meta id="id-social-description" name="description"  content="${
-                  data.description
+                  goal.description
                 }">             
                 <meta property="og:type" content="website">
-                <meta property="og:url" content="${`https://d136953gtttd92.cloudfront.net/share/toc/${req.params.userId}/${req.params.id}`}" />
-                <meta property="og:title" content="${data.name}" />
-                <meta property="og:description" content="${data.description}" />
-                <meta property="og:image" content="${data.appIcon}" />  
+                <meta property="og:url" content="${`https://d136953gtttd92.cloudfront.net/share/goal/${req.params.userId}/${req.params.id}`}" />
+                <meta property="og:title" content="${goal.name}" />
+                <meta property="og:description" content="${goal.description}" />
+                <meta property="og:image" content="${goal.contentProgress[0].appIcon}" />  
                 <meta property="og:image:secure_url" content="${
-                  data.appIcon
+                   goal.contentProgress[0].appIcon
                 }" />        
             
                 <meta property="twitter:card" content="summary_large_image" />
-                <meta property="twitter:url" content="${`https://d136953gtttd92.cloudfront.net/share/toc/${req.params.userId}/${req.params.id}`}" />
-                <meta property="twitter:title" content="${data.name}" />
+                <meta property="twitter:url" content="${`https://d136953gtttd92.cloudfront.net/share/goal/${req.params.userId}/${req.params.id}`}" />
+                <meta property="twitter:title" content="${goal.name}" />
                 <meta property="twitter:description" content="${
-                  data.description
+                  goal.description
                 }" />
-                <meta property="twitter:image" content="${data.appIcon}" />
+                <meta property="twitter:image" content="${goal.contentProgress[0].appIcon}" />
                 <style>
                   .social-card {
                     box-shadow: 0 0 0 1px rgba(0,0,0,.15), 0 2px 3px rgba(0,0,0,.2);
@@ -91,11 +100,11 @@ router.get("/goal/:userId/:id", function (req, res) {
               </head>
               <body>
                 <div class="social-card">
-                  <a href="https://d136953gtttd92.cloudfront.net/app/toc/${req.params.id}/overview">
-                    <img src="${data.appIcon}" alt="${data.name}"  class="social-card-img" />
+                  <a href="https://d136953gtttd92.cloudfront.net/app/goals/me/all">
+                    <img src="${goal.contentProgress[0].appIcon}" alt="${goal.name}"  class="social-card-img" />
                     <div class="sub-card">
-                      <p class="title">${data.name}</p>
-                      <p class="desc">${data.description}</p>
+                      <p class="title">${goal.name}</p>
+                      <p class="desc">${goal.description}</p>
                     </div>  
                   </a>   
                 </div>                       
