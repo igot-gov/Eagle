fd748d62d15f771c173af796934fe69792cb3a4a
christopher.fernandes <christyabcd.05@gmail.com>
2020-12-16 16:17:06 +0530

    Task #52811: Changed the endpoint for upload url

--- apps/ui-proxy/src/proxies_v8/proxies_v8.ts
+++ apps/ui-proxy/src/proxies_v8/proxies_v8.ts
@@ -62,7 +62,7 @@ proxiesV8.use('/sunbirdigot/*',
 )
 
 proxiesV8.use('/action/content/v3/upload/*',
-  proxyCreatorUpload(express.Router(), `http://content-service:9000/content/v3/uploadurl`)
+  proxyCreatorUpload(express.Router(), `http://content-service:9000/content/v3/upload/url`)
 )
 
 proxiesV8.use('/action/*',
