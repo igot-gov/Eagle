a05ada5f7675330502a5dc39c4004556d0ccea4f
Jenifer Ramsingh <jenifer.ramsingh@tarento.com>
2021-02-01 16:42:04 +0530

    API Bug fixes

--- apps/spv-igot/library/ws-widget/collection/src/lib/ui-table/user-list-popup/ui-user-table-pop-up.services.ts
+++ apps/spv-igot/library/ws-widget/collection/src/lib/ui-table/user-list-popup/ui-user-table-pop-up.services.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs'
 
 const API_END_POINTS = {
-  GET_ALL_USER_BY_DEPARTMENT: '/apis/protected/v8/user/autocomplete/department/',
+  GET_ALL_USER_BY_DEPARTMENT: '/apis/protected/v8/user/autocomplete/',
 }
 
 @Injectable({
@@ -12,7 +12,6 @@ const API_END_POINTS = {
 export class UserViewPopUpService {
   constructor(private http: HttpClient) { }
   getAllUsersByDepartments(searchString: string): Observable<any> {
-    const department = { departments: ['igot', 'istm', 'iGOT', 'NPA', 'NACIN', 'LSNAA'] }
-    return this.http.post<any>(`${API_END_POINTS.GET_ALL_USER_BY_DEPARTMENT}${searchString}`, department)
+    return this.http.get<any>(`${API_END_POINTS.GET_ALL_USER_BY_DEPARTMENT}${searchString}`)
   }
 }
