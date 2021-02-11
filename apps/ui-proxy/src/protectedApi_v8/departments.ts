161e951117c38f465823722ff8706df06cd2cccc
karthik-tarento <karthikeyan.rajendran@tarento.com>
2021-02-11 11:05:57 +0530

    Updated the dept public APIs

--- apps/ui-proxy/src/protectedApi_v8/departments.ts
+++ apps/ui-proxy/src/protectedApi_v8/departments.ts
@@ -5,9 +5,8 @@ import { axiosRequestConfig } from '../configs/request.config'
 import { CONSTANTS } from '../utils/env'
 
 const API_END_POINTS = {
-    addDepartment: `${CONSTANTS.USER_PROFILE_API_BASE}/dept/`,
-    getAllDepartment: `${CONSTANTS.USER_PROFILE_API_BASE}/dept`,
-    searchDepartment: (friendlyName: string) => `${CONSTANTS.USER_PROFILE_API_BASE}/dept/search?friendlyName=${friendlyName}`,
+    getAllDepartment: `${CONSTANTS.SB_EXT_API_BASE_2}/portal/getAllDept`,
+    searchDepartment: (friendlyName: string) => `${CONSTANTS.SB_EXT_API_BASE_2}/portal/deptSearch?friendlyName=${friendlyName}`,
 }
 
 export const deptApi = Router()
@@ -16,19 +15,6 @@ const unknownError = 'Failed due to unknown reason'
 deptApi.get('/getAllDept', async (_req, res) => {
     try {
         const response = await axios.get(API_END_POINTS.getAllDepartment, axiosRequestConfig)
-        res.status(response.status).send(response.data)
-    } catch (err) {
-        res.status((err && err.response && err.response.status) || 500).send(
-            (err && err.response && err.response.data) || {
-                error: unknownError,
-            }
-        )
-    }
-})
-
-deptApi.post('/addDept', async (req, res) => {
-    try {
-        const response = await axios.post(API_END_POINTS.addDepartment, req.body, axiosRequestConfig)
         res.status(response.status).send(response.data)
     } catch (err) {
         res.status((err && err.response && err.response.status) || 500).send(
