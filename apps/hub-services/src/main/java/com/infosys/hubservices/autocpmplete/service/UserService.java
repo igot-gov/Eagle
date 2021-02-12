7aab8233351e1f195b9bff2088db4e1e705fc740
karthik-tarento <karthikeyan.rajendran@tarento.com>
2021-02-12 22:52:03 +0530

    Revert "Autocomplete API implementation"
    This reverts commit e7c00b9e0c20991d218f48862609949a070580c9.

--- apps/hub-services/src/main/java/com/infosys/hubservices/autocpmplete/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.infosys.hubservices.autocpmplete.service;
-
-import java.util.List;
-import java.util.Map;
-
-public interface UserService {
-	
-	List<Map<String, Object>> searchUsername(String userName);
-}
