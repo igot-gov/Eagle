423b624e0a48df234df1bd7c8cdbe016ece746c6
amit-kumaryadav <amtydv630@gmail.com>
2021-02-16 14:08:23 +0530

    Added email config for user creation

--- apps/ui-proxy/src/protectedApi_v8/user/profile-details.ts
+++ apps/ui-proxy/src/protectedApi_v8/user/profile-details.ts
@@ -4,6 +4,7 @@ import * as fs from 'fs'
 import { axiosRequestConfig, axiosRequestConfigLong, axiosRequestConfigVeryLong } from '../../configs/request.config'
 import { IPersonalDetails, ISBUser, ISunbirdbUserResponse, IUser } from '../../models/user.model'
 import { CONSTANTS } from '../../utils/env'
+import { KEYCLOAK_USER_CREATION } from '../../utils/keycloak-user-creation'
 import { logError, logInfo } from '../../utils/logger'
 import { ERROR } from '../../utils/message'
 import {
@@ -267,7 +268,13 @@ profileDeatailsApi.post('/createUser', async (req, res) => {
                         email: sbemail_, firstName: sbfirstName_, lastName: sblastName_,
                         userId: sBuserId,
                     }
+                    try {
+                        await KEYCLOAK_USER_CREATION.sendActionsEmail(sBuserId)
+                    } catch (err) {
+                        logError('ERROR ON sendActionsEmail', err)
+                    }
                     res.send(sbUserProfileResponse)
+
                 }
 
             }
